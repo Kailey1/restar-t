9912c3ef9caf5c0192644d0477852d1e26469d0d
gracewgao <ggaoww@gmail.com>
2020-07-31 23:49:01 -0400

    onboarding drinks! and some other stuff

--- app/src/main/java/cooldudes/restart/DashboardFragment.java
+++ app/src/main/java/cooldudes/restart/DashboardFragment.java
@@ -10,8 +10,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Date;
+
+import cooldudes.restart.model.AppUser;
+
+import static cooldudes.restart.LoginActivity.user;
+import static cooldudes.restart.MainActivity.fireRef;
+import static cooldudes.restart.model.AppUser.findDiff;
 
 public class DashboardFragment extends Fragment {
+
+    private TextView goalTV, streakTV, tMinusTV, progressTV;
 
     public DashboardFragment() {
         // Required empty public constructor
@@ -23,6 +39,25 @@ public class DashboardFragment extends Fragment {
 
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_dashboard, null);
+        goalTV = v.findViewById(R.id.daily_goal);
+        streakTV = v.findViewById(R.id.streak);
+        tMinusTV = v.findViewById(R.id.tminus);
+        progressTV = v.findViewById(R.id.percent);
+
+        fireRef.child("users").child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                AppUser u = dataSnapshot.getValue(AppUser.class);
+
+                streakTV.setText(findDiff(u.getStreakStart(), new Date().getTime()) + " days");
+                goalTV.setText(u.getDailyLimit() + " mL");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         ImageButton logoutBTN = v.findViewById(R.id.logout_button);
         logoutBTN.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/cooldudes/restart/OnboardingActivity.java
+++ app/src/main/java/cooldudes/restart/OnboardingActivity.java
@@ -6,28 +6,69 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import static android.text.TextUtils.isEmpty;
 import static cooldudes.restart.LoginActivity.appUser;
 
 public class OnboardingActivity extends AppCompatActivity {
 
     private Button nextBTN;
-
+    private EditText beerET, wineET, whiskeyET, liquorET, otherET, concET;
+    private int beer, wine, whiskey, liquor, other, total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
 
+        beerET = findViewById(R.id.beer_amt);
+        wineET = findViewById(R.id.wine_amt);
+        whiskeyET = findViewById(R.id.whiskey_amt);
+        liquorET = findViewById(R.id.liquor_amt);
+        otherET = findViewById(R.id.other_amt);
+        concET = findViewById(R.id.other_conc);
+
         nextBTN = findViewById(R.id.onboarding_next);
         nextBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (!isEmpty(beerET.getText().toString())){
+                    beer = Integer.parseInt(beerET.getText().toString());
+                    total += beer*0.06;
+                }
+                if (!isEmpty(wineET.getText().toString())){
+                    wine = Integer.parseInt(wineET.getText().toString());
+                    total += wine*0.18;
+                }
+                if (!isEmpty(whiskeyET.getText().toString())){
+                    whiskey = Integer.parseInt(whiskeyET.getText().toString());
+                    total += whiskey*0.43;
+                }
+                if (!isEmpty(liquorET.getText().toString())){
+                    liquor = Integer.parseInt(liquorET.getText().toString());
+                    total += liquor*0.5;
+                }
+                if (!isEmpty(otherET.getText().toString()) || !isEmpty(concET.getText().toString())){
+                    if ((!isEmpty(otherET.getText().toString()) && !isEmpty(concET.getText().toString()))){
+                        other = Integer.parseInt(otherET.getText().toString());
+                        float conc = Float.parseFloat(concET.getText().toString());
+                        total += (other*conc)/100;
+                    } else {
+                        Toast.makeText(OnboardingActivity.this, "Make sure you fill out both the amount and concentration!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
 
+                // records total daily ABV
+                appUser.setStartAmt(total);
+
+                // moves to next screen
                 Intent i = new Intent(OnboardingActivity.this, OnboardingActivity2.class);
                 startActivity(i);
             }
         });
+
     }
 
 
